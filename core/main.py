242dcee719eb7f0ac66c0dec9b6a966a95bb51f5
MohsenSarabi <mohsen.sarabi81@gmail.com>
2025-08-23 19:48:37 +0330

    join-channel

--- core/main.py
+++ core/main.py
@@ -12,90 +12,120 @@ telebot.logger.setLevel(logging.INFO)
 API_TOKEN=os.environ.get("API_TOKEN")
 bot=telebot.TeleBot(API_TOKEN)
 
+# add channel setting
+CHANEL_ID=os.environ.get("CHANEL_ID")
+
+# status of user(average or product)
 user_status={}
+
+# is joined to channel
+is_joined=False
+
+# join to channel
+def is_member(message):
+    user_info=bot.get_chat_member(CHANEL_ID,message.from_user.id)
+    if not user_info.status in ["administrator","creator","member"]:
+        bot.send_message(chat_id=message.chat.id,text="""ااااام دوست من شرمنده ام😅😬
+                         \nولی قبل از اینکه دستورات شما را نجام بدم باید عضو کانال زیر شوید
+                         \nمنم مثل خودت از حرف زور خوشم نمیاد😤
+                         \nولی چاره ای نیست، خوشحال میشم بعد از اینکه عضو کانال زیر شدی با هم همکاری کنیم❤️
+                         \n https://t.me/+9UeYvNDCL18yZThk""")
+        is_joined=False
+        return is_joined
+    else:
+        is_joined=True
+        return is_joined
 
 # command start
 @bot.message_handler(commands=['start'])
 def start_command(message):
-    markup=InlineKeyboardMarkup()
-    button_about_us=InlineKeyboardButton("درباره ما",callback_data="about_us")
-    button_get_avg=InlineKeyboardButton("گرفتن حدود قیمت کالا",callback_data="avg")
-    button_get_10_product=InlineKeyboardButton("نمونه کالا",callback_data="product")
-    button_guide=InlineKeyboardButton("راهنما",callback_data="guide")
-    button_support=InlineKeyboardButton("پشتیبانی",callback_data="support")
+    is_joined=is_member(message)
+    if is_joined:
+        markup=InlineKeyboardMarkup()
+        button_about_us=InlineKeyboardButton("درباره ما",callback_data="about_us")
+        button_get_avg=InlineKeyboardButton("گرفتن حدود قیمت کالا",callback_data="avg")
+        button_get_10_product=InlineKeyboardButton("نمونه کالا",callback_data="product")
+        button_guide=InlineKeyboardButton("راهنما",callback_data="guide")
+        button_support=InlineKeyboardButton("پشتیبانی",callback_data="support")
 
 
-    
-    markup.add(button_about_us,button_get_avg,button_get_10_product,button_guide,button_support)
+        
+        markup.add(button_about_us,button_get_avg,button_get_10_product,button_guide,button_support)
 
-    bot.reply_to(message,text=f"""سلام {message.from_user.username} عزیز\n به ربات دیوار خیلی خوش آمدی🔥
-                 \nوظیفه من این هست که سرویس هایی از سایت دیوار را به شما ارائه دهم
-                 \nحالا به من بگو چه کمکی از دستم ساخته است🙏"""
-                 ,reply_markup=markup)
+        bot.reply_to(message,text=f"""سلام {message.from_user.username} عزیز\n به ربات دیوار خیلی خوش آمدی🔥
+                    \nوظیفه من این هست که سرویس هایی از سایت دیوار را به شما ارائه دهم
+                    \nحالا به من بگو چه کمکی از دستم ساخته است🙏"""
+                    ,reply_markup=markup)
 
 # Response buttons
 @bot.callback_query_handler(func= lambda call:True)
 def response_buttons(call):
-    if call.data=="about_us":
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,
-                              text="""خب دوست من همونطور که قبلا اشاره کرده بودم من ربات دیوار هستیم
-                              \nوظیفه من این است که برای اینکه شما راحت باشی و لازم نباشه بری تو گوگل و دیوار را سرچ کنی و...
-                              \nآقا جان من اینجام😅
-                              \nغصه چی را می خوری؟😁
-                              \nفقط لب ترک کن و دیتایی که از دیوار می خوای را به من بگو تا بهت تحویل بدم🔥
-                              \nکسی که من را ساخته اسمش محسن سرابی هستش البته بگم هااا زیاد باهاش حال نمی کنم😒
-                              \nولی خب با این حال اگر خواستی به پورتفولیو آن یک سر بزن، بچه بدی نیست😜
-                              \nhttps://www.mhsrbi.ir/
-                              """)
+    is_joined=is_member(call.message)
+    if is_joined:
+        if call.data=="about_us":
+            bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,
+                                text="""خب دوست من همونطور که قبلا اشاره کرده بودم من ربات دیوار هستیم
+                                \nوظیفه من این است که برای اینکه شما راحت باشی و لازم نباشه بری تو گوگل و دیوار را سرچ کنی و...
+                                \nآقا جان من اینجام😅
+                                \nغصه چی را می خوری؟😁
+                                \nفقط لب ترک کن و دیتایی که از دیوار می خوای را به من بگو تا بهت تحویل بدم🔥
+                                \nکسی که من را ساخته اسمش محسن سرابی هستش البته بگم هااا زیاد باهاش حال نمی کنم😒
+                                \nولی خب با این حال اگر خواستی به پورتفولیو آن یک سر بزن، بچه بدی نیست😜
+                                \nhttps://www.mhsrbi.ir/
+                                """)
 
-    if call.data=="guide":
-       bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,
-                              text="""خب ظاهرا به یکم راهنمایی نیاز داری برای کار کردن با من🧐
-                              \n/price:اگر این کامند را بزنی یعنی یک حدود قیمتی می خوای برای محصولی که الان تو ذهنت هست و خب من هم بعد از اینکه محصول  مدنظرت را گفتی اون رنج قیمتش را بهت میگم
-                              \n/product:با زدن این کامند و در ادامه با دادن  محصول مدنظر چند تا محصول از آن چیزی که انتخاب کردی را برات می فرستم
-                              \n/about:با زدن این کامند اطلاعاتی در خصوص من و توسعه دنده من دریافت می کنی🙈
-                              \n/guide:اینم از کامند راهنما هستش،می خوای اینو بزنییی؟خب من که الان راهنماییت کردم😐
-                              """) 
-    
-    if call.data=="product":
-        user_status[call.message.chat.id]="product"
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,text="""خب پس چرا معطلی؟!😭
-                              \nقصد خرید چه محصولی را داری؟
-                              \nاسم آن را برام بنویس
-                              """)
-    
-    if call.data=="avg":
-        user_status[call.message.chat.id]="avg"
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,text="""راجب چه محصولی می خوای رنج قیمتیش را بدونی؟🤓
-                              \nآن محصول را برام بنویس
-                              """)
+        if call.data=="guide":
+           bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,
+                                text="""خب ظاهرا به یکم راهنمایی نیاز داری برای کار کردن با من🧐
+                                \n/price:اگر این کامند را بزنی یعنی یک حدود قیمتی می خوای برای محصولی که الان تو ذهنت هست و خب من هم بعد از اینکه محصول  مدنظرت را گفتی اون رنج قیمتش را بهت میگم
+                                \n/product:با زدن این کامند و در ادامه با دادن  محصول مدنظر چند تا محصول از آن چیزی که انتخاب کردی را برات می فرستم
+                                \n/about:با زدن این کامند اطلاعاتی در خصوص من و توسعه دنده من دریافت می کنی🙈
+                                \n/guide:اینم از کامند راهنما هستش،می خوای اینو بزنییی؟خب من که الان راهنماییت کردم😐
+                                """) 
         
-    if call.data=="support":
-        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,text="""اوه، اوه😬
-                              \n مثل اینکه کار ضروری برات پیش اومده که با پشتیبانم کار داری
-                              \nاین ID پشتیبانم در تلگرام هستش
-                              \n@sb_mohsen""")
+        if call.data=="product":
+            user_status[call.message.chat.id]="product"
+            bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,text="""خب پس چرا معطلی؟!😭
+                                \nقصد خرید چه محصولی را داری؟
+                                \nاسم آن را برام بنویس
+                                """)
+        
+        if call.data=="avg":
+            user_status[call.message.chat.id]="avg"
+            bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,text="""راجب چه محصولی می خوای رنج قیمتیش را بدونی؟🤓
+                                \nآن محصول را برام بنویس
+                                """)
+            
+        if call.data=="support":
+            bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.id,text="""اوه، اوه😬
+                                \n مثل اینکه کار ضروری برات پیش اومده که با پشتیبانم کار داری
+                                \nاین ID پشتیبانم در تلگرام هستش
+                                \n@sb_mohsen""")
 
 # get avg price of product
 @bot.message_handler(commands=['avg'])   
 def get_average(message):
-    user_status[message.chat.id]="avg"
-    bot.send_message(chat_id=message.chat.id,text="""راجب چه محصولی می خوای رنج قیمتیش را بدونی؟🤓
-                              \nآن محصول را برام بنویس
-                              """)     
+    is_joined=is_member(message)
+    if is_joined:
+        user_status[message.chat.id]="avg"
+        bot.send_message(chat_id=message.chat.id,text="""راجب چه محصولی می خوای رنج قیمتیش را بدونی؟🤓
+                                \nآن محصول را برام بنویس
+                                """)     
 
 # get latest products
 @bot.message_handler(commands=['product'])   
 def get_average(message):
-    user_status[message.chat.id]="product"
-    bot.send_message(chat_id=message.chat.id,text="""خب پس چرا معطلی؟!😭
-                              \nقصد خرید چه محصولی را داری؟
-                              \nاسم آن را برام بنویس
-                              """)     
+    is_joined=is_member(message)
+    if is_joined:
+        user_status[message.chat.id]="product"
+        bot.send_message(chat_id=message.chat.id,text="""خب پس چرا معطلی؟!😭
+                                \nقصد خرید چه محصولی را داری؟
+                                \nاسم آن را برام بنویس
+                                """)     
     
 
 # structure of getting latest products and avg
-@bot.message_handler(func= lambda message:True)
+@bot.message_handler(func=lambda message:is_member(message))
 def get_product_info(message):
     chat_id=message.chat.id
     query = message.text.strip()
